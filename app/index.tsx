cb599f47162c3be26e7223bb95aee1390415ef6c
paltansjosess <paltansjosess@gmail.com>
2025-10-06 02:51:33 +0000

    💄 (ui): Cuadros para llenar con texto -Se agrega los textinput para crear un espacio para llenar con datos -Se sigue personalizando, cambios de colores

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,12 @@ import { View, Text, TextInput, Pressable, Image, ImageBackground } from "react-
 import React, { useState } from "react";
 
 export default function Index() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSignUp = () => {
+    console.log("Sign Up with:", email, password);
+    // Aquí iría la lógica de Firebase
+  };
   
   return (
     <ImageBackground 
@@ -14,6 +20,22 @@ export default function Index() {
 
       <View className="bg-black/50 p-10 w-80 items-center shadow-xl">
         <Text className="text-3xl font-bold text-[#B8EDFF] mb-6">Sign Up</Text>
+      
+      <TextInput
+          className="w-full p-3 rounded-xl bg-[#8AE2FF] border-[#2ECBFF] mb-4"
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        />
+
+        <TextInput
+          className="w-full p-3 rounded-xl bg-[#8AE2FF] border-[#2ECBFF] mb-6"
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+
       </View>
     </ImageBackground>
   );
